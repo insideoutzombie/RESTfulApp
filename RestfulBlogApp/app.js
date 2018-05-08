f32d5ce8c585fed9849cfbfbf3692f1562dce4c1
insideoutzombie <david@web3devs.com>
2018-05-07 21:11:29 -0500

    add much more content

--- RestfulBlogApp/app.js
+++ RestfulBlogApp/app.js
@@ -2,3 +2,58 @@ bodyParser = require("body-parser"),
 mongoose   = require("mongoose"),
 express    = require("express"),
 app        = express();
+
+// APP CONFIG
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Mongoose/Model Config
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+// Restful Routes
+app.get("/", function(req, res){
+    res.redirect("/blogs");
+});
+
+// INDEX route
+app.get("/blogs", function(req, res){
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log("ERROR");
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    });
+});
+// NEW Route
+app.get("/blogs/new", function(req, res){
+    res.render("new");
+});
+
+// CREATE ROUTE
+app.post("/blogs", function(req, res){
+//create blog
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            res.render("new");
+        } else {
+//then, redirect to the index
+            res.redirect("/blogs");
+        }
+    });
+
+})
+
+
+app.listen(3000, function(){
+    console.log("The Server has Started!");
+});
