bbc8349d86f7adf50f17ede89c65224500886759
insideoutzombie <david@web3devs.com>
2018-05-09 16:36:11 -0500

    add lot more code to the project

--- RestfulBlogApp/app.js
+++ RestfulBlogApp/app.js
@@ -1,13 +1,15 @@
-bodyParser = require("body-parser"),
-mongoose   = require("mongoose"),
-express    = require("express"),
-app        = express();
+methodOverride = require("method-override"),
+bodyParser     = require("body-parser"),
+mongoose       = require("mongoose"),
+express        = require("express"),
+app            = express();
 
 // APP CONFIG
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 // Mongoose/Model Config
 var blogSchema = new mongoose.Schema({
@@ -63,6 +65,27 @@ app.get("/blogs/:id", function(req, res){
     });
 });
 
+// EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+  Blog.findById(req.params.id, function(err, foundBlog){
+    if(err){
+      res.redirect("/blogs");
+    } else {
+      res.render("edit", {blog: foundBlog});
+    }
+  });
+});
+
+// UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+   Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+     if(err){
+       res.redirect("/blogs");
+     } else {
+       res.redirect("/blogs/" + req.params.id);
+     }
+   });
+});
 
 app.listen(3000, function(){
     console.log("The Server has Started!");
