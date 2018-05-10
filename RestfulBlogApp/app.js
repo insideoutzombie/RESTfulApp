d4e27e8d0ed80e177d7848362b58d5684ab46035
insideoutzombie <david@web3devs.com>
2018-05-09 19:44:33 -0500

    final touches maybe

--- RestfulBlogApp/app.js
+++ RestfulBlogApp/app.js
@@ -1,8 +1,9 @@
-methodOverride = require("method-override"),
-bodyParser     = require("body-parser"),
-mongoose       = require("mongoose"),
-express        = require("express"),
-app            = express();
+expressSanatizer = require("express-sanitizer"),
+methodOverride   = require("method-override"),
+bodyParser       = require("body-parser"),
+mongoose         = require("mongoose"),
+express          = require("express"),
+app              = express();
 
 // APP CONFIG
 mongoose.connect("mongodb://localhost/restful_blog_app");
@@ -10,6 +11,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.use(expressSanatizer());
 
 // Mongoose/Model Config
 var blogSchema = new mongoose.Schema({
@@ -44,6 +46,10 @@ app.get("/blogs/new", function(req, res){
 // CREATE ROUTE
 app.post("/blogs", function(req, res){
 //create blog
+console.log(req.body);
+req.body.blog.body = req.sanitize(req.body.blog.body);
+console.log("============");
+console.log(req.body);
     Blog.create(req.body.blog, function(err, newBlog){
         if(err){
             res.render("new");
@@ -78,6 +84,7 @@ app.get("/blogs/:id/edit", function(req, res){
 
 // UPDATE ROUTE
 app.put("/blogs/:id", function(req, res){
+  req.body.blog.body = req.sanitize(req.body.blog.body);
    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
      if(err){
        res.redirect("/blogs");
@@ -85,6 +92,19 @@ app.put("/blogs/:id", function(req, res){
        res.redirect("/blogs/" + req.params.id);
      }
    });
+});
+
+// DELETE ROUTE
+app.delete("/blogs/:id", function(req, res){
+// destroy blog
+Blog.findByIdAndRemove(req.params.id, function(err){
+  if(err){
+    res.redirect("/blogs");
+  } else {
+    res.redirect("/blogs");
+  }
+});
+// redirect somewhere
 });
 
 app.listen(3000, function(){
